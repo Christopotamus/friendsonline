268cf9ec4937d857cd5ebdb033acd106af0cbef8
Christopher Budd <Chris.cyborg@gmail.com>
2016-02-19 22:47:32 -0800

    Going to just use secret key base instead of specific for devise

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,7 +60,9 @@ namespace :deploy do
   task :migrate do
     on roles(:web) do
       within release_path do
-        execute :rake, 'db:migrate'
+        with rails_env: :production do
+          execute :rake, 'db:migrate'
+        end
       end
     end
   end
@@ -74,7 +76,9 @@ namespace :deploy do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       within release_path do
-        execute :rake, 'assets:precompile'
+        with rails_env: :production do
+          execute :rake, 'assets:precompile'
+        end
       end
     end
   end
